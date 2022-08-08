856ee56118d44e960c0624e976732a7c25702d77
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-08-08 01:03:12 -0300

    Videoaula 3 5:41

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'user.name' => 'required',
+            'user.email' => ['required', 'email'],
+            'user.cpf' => 'required',
+            'user.password' => ['required', 'min:8'],
+            'phones.0.number' => ['required', 'size:14'],
+            'phones.1.number' => ['required', 'size:15'],
+            'address.cep' => 'required',
+            'address.uf' => ['required', 'size:2'],
+            'address.city'
+        ];
+    }
+}
